b09404c78edd8c7f2cf1f029836b28fcbe8cf58f
Ahmed Nouira <ahmnouira@gmail.com>
2021-08-08 00:11:35 +0100

    refactor: change folder structure

---
+++ src/theme/index.ts
@@ -0,0 +1,16 @@
+import {ThemeType} from './theme'
+import {ThemeContext, ThemeContextProps, ThemeProvideProps, ThemeProvider} from './theme.context'
+
+
+export type {
+    ThemeType,
+    ThemeContextProps, 
+    ThemeProvideProps
+
+}
+
+export {
+    ThemeContext,
+    ThemeProvider
+}
+
---
+++ src/ui/index.tsx
@@ -0,0 +1,5 @@
+import {Container} from './Container'
+import {Bold} from './Bold'
+import {EmptyHeader} from './EmptyHeader'
+
+export {Container, Bold, EmptyHeader}
