5d94288e50143dcc733507a425c786043c643028
Ahmed Nouira <ahmnouira@gmail.com>
2021-08-08 05:52:57 +0100

    feat: add example

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Bold, Container, KeyboardAvoiding, Toast, theme, ThemeProvider, Status} from "react-native-innovant"
+import { Bold, Container, KeyboardAvoiding, theme, ThemeProvider, Status, Pill, BtnWhite, Box, CheckBox, BoxList,SwitchField, RadioBox, ErrorState } from "react-native-innovant"
 
 export const App = () => {
 return(
   <ThemeProvider theme={theme}>
-  <Container>
-    <Status backgroundColor="red" barStyle="dark-content" />
+    <Status backgroundColor="white" barStyle="dark-content" />
     <KeyboardAvoiding>
+      <Container secondary>
+      
       <Bold variant="primary">This is a Bold</Bold>
-    </KeyboardAvoiding>
-    <Toast visible message="sdsdsds" />
-  </Container>
+      <Pill  title="music" isChecked  handlePress={() => {}} />
+      <BtnWhite title="Submit" />
+      <Box title="dsds" pic={{}} handlePress={() => {}}  isChecked={false}/>
+      <SwitchField label="sdsdsd" />
+      <RadioBox color="red" />
+      <CheckBox checked color="blue" />
+      <ErrorState error="sdsdsdsds" />
+      </Container>
+    </KeyboardAvoiding> 
   </ThemeProvider>
 )
 };
