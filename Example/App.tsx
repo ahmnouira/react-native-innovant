aef195897b92fdca6010100860e24e5d2f490a25
Ahmed Nouira <ahmnouira@gmail.com>
2021-08-08 02:21:44 +0100

    fix: babel presets

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,1 +1,12 @@
-export const App = () => {};
+import React from 'react'
+import { Bold, Container, KeyboardAvoiding, IconButton} from 'react-native-innovant'
+
+export const App = () => {
+
+  <Container secondary>
+    <KeyboardAvoiding>
+      <Bold variant="primary">This is a Bold</Bold>
+      <IconButton icon="edit" color="red" size={20} onPress={() => {}}></IconButton>
+    </KeyboardAvoiding>
+  </Container>
+};
