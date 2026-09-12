42f5bea907e42c126c61e5cf47da718445cdfa6b
Ahmed Nouira <ahmnouira@gmail.com>
2026-09-12 12:14:19 +0100

    test: fixed jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/src/**/*.spec.tsx'],
   setupFiles: ['<rootDir>/jest.setup.js'],
